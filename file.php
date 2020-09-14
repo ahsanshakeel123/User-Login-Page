3d652034abfbf1bf57fdd3dc2fc98bdbb4e6e520
Ahsan Shakeel <65927781+ahsanshakeel123@users.noreply.github.com>
2020-09-14 22:45:44 +0500

    Update file.php

--- file.php
+++ file.php
@@ -13,16 +13,16 @@ if(isset($_POST['username'])){
     $uname=$_POST['username'];
     $password=$_POST['password'];
     
-    $sql="select * from loginform where user='".$uname."'AND Pass='".$password."' limit 1";
+    $sql="select * from loginform where user='".$username."'AND Pass='".$password."' limit 1";
     
     $result=mysql_query($sql);
     
     if(mysql_num_rows($result)==1){
-        echo " You Have Successfully Logged in";
+        echo " You are successfully logged in";
         exit();
     }
     else{
-        echo " You Have Entered Incorrect Password";
+        echo " Sorry Login Is not possible";
         exit();
     }
         
